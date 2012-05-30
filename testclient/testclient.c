b3b0c343fb4b37ebbdfaa8804ea3e5fc51ceab23
Ger Hobbelt <ger@hobbelt.com>
2012-05-30 04:11:10 +0200

    multiple fixes  and tweaks to the test client: it was quickly overloading my 4 core server thanks to an absurd number of printf()+send() calls; the first iteration changed this to using send() only with constant strings, but profiling showed that now the CPU load was entirely in System, i.e. the kernel. The second iteration is shown here: a large buffer is prepared with constant data, which is accessible by every thread for pumping across the line.
    Also, the threads are not suspended when the 15 seconds of the test expire, but are told to 'bugger off', i.e. abort. Thanks to using select() instead of polling the socket like crazy, this is easily done with minimal load by the test app during tests.
    
    Further fixes: using the correct (64-bit) types for the CPU affinity mask + corrected the modulo logic as well (shift-left by 999? heh. :-) )

--- testclient/testclient.c
+++ testclient/testclient.c
@@ -34,15 +34,40 @@ int sockprintf(SOCKET soc, const char * fmt, ...) {
   return ret;
 }
 
+static char *source_data_buffer = NULL;
+static size_t source_data_size = 0;
+
+static void send_dummy_data(SOCKET soc, size_t len)
+{
+	size_t i, l;
+
+	for (i = len; i > 0; ) 
+	{
+		int rv;
+
+		l = i;
+		if (l > source_data_size)
+			l = source_data_size;
+		rv = send(soc, source_data_buffer, l, 0);
+		if (rv <= 0)
+		{
+			printf("**BONK**! in send_dummy_data()\a\n");
+			return;
+		}
+		i -= l;
+	} 
+}
 
 static struct sockaddr_in target = {0};
 static CRITICAL_SECTION cs = {0};
 static size_t expectedData = 0;
-static DWORD availableCPUs = 1;
-static DWORD totalCPUs = 1;
+static DWORD_PTR availableCPUs = 1;
+static DWORD_PTR totalCPUs = 1;
 static unsigned good = 0;
 static unsigned bad = 0;
 unsigned long postSize = 0;
+static int verbose = 1;
+static int volatile bugger_off = 0;
 
 
 int WINAPI ClientMain(void * clientNo) {
@@ -52,11 +77,11 @@ int WINAPI ClientMain(void * clientNo) {
   size_t totalData = 0;
   int isBody = 0;
   int isTest = (clientNo == 0);
-  int cpu = ((int)clientNo) % 1000;
+  int cpu = ((int)clientNo) % 64; /* Win32: max 64 processors possible for affinity mask!!! See the Win32 API docs. */
   int timeOut = 10;
 
-  if ((!isTest) && (((1<<cpu) & availableCPUs)!=0)) {
-    SetThreadAffinityMask(GetCurrentThread(), 1<<cpu);
+  if ((!isTest) && (((1ULL<<cpu) & availableCPUs)!=0)) {
+    SetThreadAffinityMask(GetCurrentThread(), 1ULL<<cpu);
   }
 
   soc = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -82,7 +107,7 @@ int WINAPI ClientMain(void * clientNo) {
 
   // "GET" with 10000 bytes extra head data
   // sockprintf(soc, "GET %s HTTP/1.1\r\nHost: %s\r\nConnection: Close\r\n", RESOURCE, HOST);
-  // {int i; for (i=0;i<(10000/25);i++) {sockprintf(soc, "Comment%04u: 1234567890\r\n");}}
+  // send_dummy_data(soc, 10000);
   // sockprintf(soc, "\r\n");
 
   // "GET" with 2000 bytes of query string
@@ -92,12 +117,12 @@ int WINAPI ClientMain(void * clientNo) {
 
   // "POST <postSize> bytes"
   sockprintf(soc, "POST %s HTTP/1.1\r\nHost: %s\r\nConnection: Close\r\nContent-Length: %u\r\n\r\n", RESOURCE, HOST, postSize);
-  {unsigned long i; for (i=0;i<postSize/10;i++) {sockprintf(soc, "1234567890");} for (i=0;i<postSize%10;i++) {sockprintf(soc, ".");}}
+  send_dummy_data(soc, postSize);
   timeOut += postSize/10000;
 
   // "POST" with 2000 bytes of query string
   // sockprintf(soc, "POST %s?", RESOURCE);
-  // {int i; for (i=0;i<200;i++) {sockprintf(soc, "1234567890");}}
+  // send_dummy_data(soc, 2000);
   // sockprintf(soc, " HTTP/1.1\r\nHost: %s\r\nConnection: Close\r\nContent-Length: 0\r\n\r\n", HOST);
 
 
@@ -106,11 +131,26 @@ int WINAPI ClientMain(void * clientNo) {
     char buf[2048];
     int chunkSize = 0;
     unsigned long dataReady = 0;
+	FD_SET fds;
+	struct timeval tv;
+	int srv;
 
-    Sleep(1);
+	tv.tv_sec = 1;
+	tv.tv_usec = 0;
+
+	FD_ZERO(&fds);
+	FD_SET(soc, &fds);
+	srv = select(1, &fds, 0, 0, &tv);
+	if (bugger_off)
+	{
+		if (verbose == 1) fputc('~', stdout);
+		else if (verbose > 1) printf("Closing prematurely: server is taking too long to our taste: client %i --> %u/%u\r\n", (int)clientNo, (unsigned int)totalData, (unsigned int)postSize);
+		break;
+	}
 
     if (ioctlsocket(soc, FIONREAD, &dataReady) < 0) break;
     if (dataReady) {
+	  if (verbose > 1) fputc('+', stdout); // see a bit of action around here...
       chunkSize = recv(soc, buf, sizeof(buf), 0);
       if (chunkSize<0) {
         printf("Error: recv failed for client %i\r\n", (int)clientNo);
@@ -134,7 +174,25 @@ int WINAPI ClientMain(void * clientNo) {
       }
     } else {
       time_t current = time(0);
-      if (difftime(current, lastData) > timeOut) break;
+	  if (verbose > 1) fputc('.', stdout); // see a bit of action around here...
+      if (difftime(current, lastData) > timeOut) 
+	  {
+        printf("Error: request timed out for client %i\r\n", (int)clientNo);
+		break;
+	  }
+	  if (srv == 1)
+	  {
+		  // server closed connection:
+		  if (verbose == 1) fputc('#', stdout);
+		  else if (verbose > 1) printf("Server close: client %i --> %u/%u\r\n", (int)clientNo, (unsigned int)totalData, (unsigned int)postSize);
+		break;
+	  }
+	  else 
+	  {
+		  // server crash / abortus provocatus?
+        printf("Abortus Provocatus?: client %i --> %u/%u\r\n", (int)clientNo, (unsigned int)totalData, (unsigned int)postSize);
+		break;
+	  }
     }
   }
 
@@ -159,29 +217,50 @@ void RunMultiClientTest(int loop) {
 
   HANDLE hThread[CLIENTCOUNT] = {0};
   int i;
+  DWORD res;
 
   for (i=0;i<CLIENTCOUNT;i++) {
     DWORD dummy;
     hThread[i] = CreateThread(NULL, 1024*32, (LPTHREAD_START_ROUTINE)ClientMain, (void*)(1000*loop+i), 0, &dummy);
   }
 
-  WaitForMultipleObjects(CLIENTCOUNT, hThread, TRUE, 15000);
+  res = WaitForMultipleObjects(CLIENTCOUNT, hThread, TRUE, 15000);
+  if (res != WAIT_OBJECT_0 + CLIENTCOUNT - 1 && res != 0 /* all threads already terminated by themselves as they were done */)
+  {
+      printf("WaitForMultipleObjects() --> $%08x%s / $%08x\r\n", res, (res == STATUS_TIMEOUT ? " (STATUS_TIMEOUT)" : ""), GetLastError());
+  }
   for (i=0;i<CLIENTCOUNT;i++) {
-    if (WaitForSingleObject(hThread[i], 0)==WAIT_OBJECT_0) {
+    res = WaitForSingleObject(hThread[i], 0);
+	if (res == WAIT_OBJECT_0) {
       CloseHandle(hThread[i]);
       hThread[i]=0;
     }
+	else
+	{
+      printf("Thread %i WaitForSingleObject() --> $%08x%s / $%08x\r\n", i, res, (res == STATUS_TIMEOUT ? " (STATUS_TIMEOUT)" : ""), GetLastError());
+	}
   }
   for (i=0;i<CLIENTCOUNT;i++) {
     if (hThread[i]) {
       EnterCriticalSection(&cs);
-      SuspendThread(hThread[i]); // -> check this thread in the debugger
-      printf("Thread %i did not finish!\r\n", (int)(1000*loop+i));
+#if 0
+	  SuspendThread(hThread[i]); // -> check this thread in the debugger
+#else
+	  bugger_off = 1;
+
+		res = WaitForSingleObject(hThread[i], 2 * 1000); // wait for a maximum of 2 select() poll periods to make sure that the thread had a chance to see 'bugger_off'...
+		if (res == WAIT_OBJECT_0) {
+			CloseHandle(hThread[i]);
+			hThread[i]=0;
+		}
+#endif
+		printf("Thread %i did not finish in time!\r\n", (int)(1000*loop+i));
+
       LeaveCriticalSection(&cs);
     }
   }
   EnterCriticalSection(&cs);
-  printf("Test cylce %u completed\r\n\r\n", loop);
+  printf("Test cycle %u completed\r\n\r\n", loop);
   LeaveCriticalSection(&cs);
 }
 
@@ -214,6 +293,7 @@ int MultiClientTestAutomatic(unsigned long initialPostSize) {
       fprintf(log, "%u\t%u\t%u\r\n", postSize, good, bad);
       fclose(log);
     }
+	Sleep(1000);
 
     postSize = (postSize!=0) ? (postSize<<1) : 1;
 
@@ -238,7 +318,7 @@ int SingleClientTestAutomatic(void) {
     }
     log = fopen("testclient.log", "at");
     if (log) {
-      fprintf(log, "Cylce<%u>\t%u\t%u\r\n", cycle, good, bad);
+      fprintf(log, "Cycle<%u>\t%u\t%u\r\n", cycle, good, bad);
       fclose(log);
     }
     printf("test cycle %u: %u good, %u bad\r\n", cycle, good, bad);
@@ -272,6 +352,22 @@ int main(int argc, char * argv[]) {
   printf("CPUs (bit masks): process=%x, system=%x\r\n", availableCPUs, totalCPUs);
 
   InitializeCriticalSectionAndSpinCount(&cs, 100000);
+
+  /* set up the data buffer for fast I/O */
+  source_data_size = 1024 * 1024;
+  source_data_buffer = (char *)malloc(source_data_size);
+  {
+	  int i;
+	  char *d = source_data_buffer;
+
+	  for (i = source_data_size; i >= 80; )
+	  {
+		  _snprintf(d, i, "Comment%04u: 1234567890\r\n", i % 10000);
+		  i -= strlen(d);
+		  d += strlen(d);
+	  }
+	  memset(d, '.', i);
+  }
 
   /* Do the actual test here */
   MultiClientTestAutomatic(200000);
