88e8ab0da03290720102108abf4075fb834b08d2
Ger Hobbelt <ger@hobbelt.com>
2012-06-02 21:23:58 +0200

    fixed crash in chat example's JavaScript due to typeof(data)=='undefined' under some circumstances.

--- examples/html/main.js
+++ examples/html/main.js
@@ -16,25 +16,27 @@ chat.normalizeText = function(text) {
 };
 
 chat.refresh = function(data) {
-  $.each(data, function(index, entry) {
-    var row = $('<div>').addClass('message-row').appendTo('#mml');
-    var timestamp = (new Date(entry.timestamp * 1000)).toLocaleTimeString();
-    $('<span>')
-      .addClass('message-timestamp')
-      .html('[' + timestamp + ']')
-      .prependTo(row);
-    $('<span>')
-      .addClass('message-user')
-      .addClass(entry.user ? '' : 'message-user-server')
-      .html(chat.normalizeText((entry.user || '[server]') + ':'))
-      .appendTo(row);
-    $('<span>')
-      .addClass('message-text')
-      .addClass(entry.user ? '' : 'message-text-server')
-      .html(chat.normalizeText(entry.text))
-      .appendTo(row);
-    chat.lastMessageId = Math.max(chat.lastMessageId, entry.id) + 1;
-  });
+  if (data) {
+    $.each(data, function(index, entry) {
+      var row = $('<div>').addClass('message-row').appendTo('#mml');
+      var timestamp = (new Date(entry.timestamp * 1000)).toLocaleTimeString();
+      $('<span>')
+        .addClass('message-timestamp')
+        .html('[' + timestamp + ']')
+        .prependTo(row);
+      $('<span>')
+        .addClass('message-user')
+        .addClass(entry.user ? '' : 'message-user-server')
+        .html(chat.normalizeText((entry.user || '[server]') + ':'))
+        .appendTo(row);
+      $('<span>')
+        .addClass('message-text')
+        .addClass(entry.user ? '' : 'message-text-server')
+        .html(chat.normalizeText(entry.text))
+        .appendTo(row);
+      chat.lastMessageId = Math.max(chat.lastMessageId, entry.id) + 1;
+    });
+  }
 
   // Keep only chat.maxVisibleMessages, delete older ones.
   while ($('#mml').children().length > chat.maxVisibleMessages) {
