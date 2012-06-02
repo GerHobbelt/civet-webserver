7710c864d177096247d182fc54e12e3108ae1acd
Ger Hobbelt <ger@hobbelt.com>
2012-06-02 14:09:30 +0200

    Win32: always provide the various _WIN32_WINNT #defines (originally available in sdkddkver.h but then you'd have to have the SDK installed)

--- mongoose_sys_porting.h
+++ mongoose_sys_porting.h
@@ -64,6 +64,37 @@
 #include <windows.h>
 #include <mswsock.h>
 
+//
+// _WIN32_WINNT version constants           <sdkddkver.h>
+//
+#ifndef _WIN32_WINNT_NT4                    
+#define _WIN32_WINNT_NT4                    0x0400
+#endif
+#ifndef _WIN32_WINNT_WIN2K                  
+#define _WIN32_WINNT_WIN2K                  0x0500
+#endif
+#ifndef _WIN32_WINNT_WINXP                  
+#define _WIN32_WINNT_WINXP                  0x0501
+#endif
+#ifndef _WIN32_WINNT_WS03                   
+#define _WIN32_WINNT_WS03                   0x0502
+#endif
+#ifndef _WIN32_WINNT_WIN6                   
+#define _WIN32_WINNT_WIN6                   0x0600
+#endif
+#ifndef _WIN32_WINNT_VISTA                  
+#define _WIN32_WINNT_VISTA                  0x0600
+#endif
+#ifndef _WIN32_WINNT_WS08                   
+#define _WIN32_WINNT_WS08                   0x0600
+#endif
+#ifndef _WIN32_WINNT_LONGHORN               
+#define _WIN32_WINNT_LONGHORN               0x0600
+#endif
+#ifndef _WIN32_WINNT_WIN7                   
+#define _WIN32_WINNT_WIN7                   0x0601
+#endif
+
 #ifndef PATH_MAX
 #define PATH_MAX MAX_PATH
 #endif
