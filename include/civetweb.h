9a072774454dfe49610f0dba62c66fbb94bbe120
xtne6f <xt4ubq@gmail.com>
2016-06-02 19:01:59 +0900

    Exclude handle_form.inl

--- include/civetweb.h
+++ include/civetweb.h
@@ -833,7 +833,6 @@ CIVETWEB_API void mg_close_connection(struct mg_connection *conn);
    Deprecated: Use mg_handle_form_request instead. */
 CIVETWEB_API int mg_upload(struct mg_connection *conn,
                            const char *destination_dir);
-#endif
 
 
 /* This structure contains callback functions for handling form fields.
@@ -927,6 +926,7 @@ enum {
  * no longer required. */
 CIVETWEB_API int mg_handle_form_request(struct mg_connection *conn,
                                         struct mg_form_data_handler *fdh);
+#endif
 
 
 /* Convenience function -- create detached thread.
