5bea1398efe5e7cabca5edcad9d3bec9f89fd7ae
xtne6f <xt4ubq@gmail.com>
2016-06-19 20:47:12 +0900

    Do not echo back user-controllable input
    Such verbose responses can cause XSS in some cases.

--- src/civetweb.c
+++ src/civetweb.c
@@ -12598,8 +12598,8 @@ handle_request(struct mg_connection *conn)
 			 * PUT/DELETE methods are not valid. */
 			mg_send_http_error(conn,
 			                   405,
-			                   "%s method not allowed",
-			                   conn->request_info.request_method);
+			                   "%s",
+			                   "Method not allowed");
 			return;
 		}
 
@@ -12772,8 +12772,8 @@ handle_request(struct mg_connection *conn)
 		 * only for scripts (Lua, CGI) and callbacks. */
 		mg_send_http_error(conn,
 		                   405,
-		                   "%s method not allowed",
-		                   conn->request_info.request_method);
+		                   "%s",
+		                   "Method not allowed");
 		return;
 	}
 
@@ -12824,8 +12824,8 @@ handle_request(struct mg_connection *conn)
 	    && (0 != strcmp(ri->request_method, "HEAD"))) {
 		mg_send_http_error(conn,
 		                   405,
-		                   "%s method not allowed",
-		                   conn->request_info.request_method);
+		                   "%s",
+		                   "Method not allowed");
 		return;
 	}
 
@@ -15787,8 +15787,7 @@ process_new_connection(struct mg_connection *conn)
 			            NULL, /* No truncation check for ebuf */
 			            ebuf,
 			            sizeof(ebuf),
-			            "Bad HTTP version: [%s]",
-			            ri->http_version);
+			            "Bad HTTP version");
 			mg_send_http_error(conn, 505, "%s", ebuf);
 		}
 
