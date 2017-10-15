58c66ca40c72b35640de3094f8ffdd592776c53b
xtne6f <xt4ubq@gmail.com>
2017-10-15 14:06:57 +0900

    Remove unused functions

--- src/civetweb.c
+++ src/civetweb.c
@@ -6498,7 +6498,7 @@ mg_get_cookie(const char *cookie_header,
 }
 
 
-#if defined(USE_WEBSOCKET) || defined(USE_LUA)
+#if defined(USE_WEBSOCKET)
 static void
 base64_encode(const unsigned char *src, int src_len, char *dst)
 {
@@ -6524,74 +6524,6 @@ base64_encode(const unsigned char *src, int src_len, char *dst)
 		dst[j++] = '=';
 	}
 	dst[j++] = '\0';
-}
-#endif
-
-
-#if defined(USE_LUA)
-static unsigned char
-b64reverse(char letter)
-{
-	if ((letter >= 'A') && (letter <= 'Z')) {
-		return letter - 'A';
-	}
-	if ((letter >= 'a') && (letter <= 'z')) {
-		return letter - 'a' + 26;
-	}
-	if ((letter >= '0') && (letter <= '9')) {
-		return letter - '0' + 52;
-	}
-	if (letter == '+') {
-		return 62;
-	}
-	if (letter == '/') {
-		return 63;
-	}
-	if (letter == '=') {
-		return 255; /* normal end */
-	}
-	return 254; /* error */
-}
-
-
-static int
-base64_decode(const unsigned char *src, int src_len, char *dst, size_t *dst_len)
-{
-	int i;
-	unsigned char a, b, c, d;
-
-	*dst_len = 0;
-
-	for (i = 0; i < src_len; i += 4) {
-		a = b64reverse(src[i]);
-		if (a >= 254) {
-			return i;
-		}
-
-		b = b64reverse(((i + 1) >= src_len) ? 0 : src[i + 1]);
-		if (b >= 254) {
-			return i + 1;
-		}
-
-		c = b64reverse(((i + 2) >= src_len) ? 0 : src[i + 2]);
-		if (c == 254) {
-			return i + 2;
-		}
-
-		d = b64reverse(((i + 3) >= src_len) ? 0 : src[i + 3]);
-		if (d == 254) {
-			return i + 3;
-		}
-
-		dst[(*dst_len)++] = (a << 2) + (b >> 4);
-		if (c != 255) {
-			dst[(*dst_len)++] = (b << 4) + (c >> 2);
-			if (d != 255) {
-				dst[(*dst_len)++] = (c << 6) + d;
-			}
-		}
-	}
-	return -1;
 }
 #endif
 
